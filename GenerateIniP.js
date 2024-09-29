0b5884594b3f1a6d57220c13b73e071b4bb88e1e
ZachWang <815810395@qq.com>
2024-09-29 22:56:02 +0800

    Update GenerateIniP.js

--- GenerateIniP.js
+++ GenerateIniP.js
@@ -1,4 +1,7 @@
 function GenerateIniP(){
+  //读取XLD_keypoints.json和SMX_keypoints.json中的数据
+  var XLD_KeyP = JSON.parse(document.getElementById('XLD_keypoints').value);
+  var SMX_KeyP = JSON.parse(document.getElementById('SMX_keypoints').value);
   //设置方案数量
   var planNum = 8;
   //设置最迟开始时间
